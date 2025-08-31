83230ee4a9bd7089ed896f9c16e36254ff35338f
Matebey <matveyveterok@gmail.com>
2025-08-31 21:16:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 import sqlite3
 
 # Токен бота от @BotFather
-BOT_TOKEN = "ВАШ_ТОКЕН_БОТА"
+BOT_TOKEN = "7953514140:AAGg-AgyL6Y2mvzfyKesnpouJkU6p_B8Zeo"
 
 # Настройки ЮMoney
 YOOMONEY_CLIENT_ID = "your_client_id"
@@ -20,7 +20,7 @@ OVPN_KEYS_DIR = "/etc/openvpn/easy-rsa/pki/"
 DB_PATH = "/opt/coffee-coma-vpn/vpn_bot.db"
 
 # Админ пользователи (замените на ваш Telegram ID)
-ADMINS = [123456789]
+ADMINS = [5631675412]
 
 # Создаем директории
 os.makedirs(OVPN_CLIENT_DIR, exist_ok=True)
@@ -38,25 +38,6 @@ def get_setting(key, default=None):
         return default
 
 # Динамические настройки
-PRICES = {
-    "1_month": int(get_setting('price', 50))
-}
-
-DNS_SERVERS = [get_setting('dns1', '8.8.8.8'), get_setting('dns2', '8.8.4.4')]
-
-OVPN_PORT = get_setting('port', '1194')
-def get_setting(key, default=None):
-    try:
-        conn = sqlite3.connect(DB_PATH)
-        cursor = conn.cursor()
-        cursor.execute("SELECT value FROM settings WHERE key = ?", (key,))
-        result = cursor.fetchone()
-        conn.close()
-        return result[0] if result else default
-    except:
-        return default
-
-# Динамические настройки (теперь будут обновляться из БД)
 PRICES = {
     "1_month": int(get_setting('price', 50))
 }
