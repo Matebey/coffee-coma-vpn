310e9af9a6fdef361b961790379d94ae02e4dec0
Matebey <matveyveterok@gmail.com>
2025-09-02 14:36:42 +0300

    Update init.py

--- bot/handlers/init.py
+++ bot/handlers/init.py
@@ -1,11 +1,11 @@
-from .start import register_start_handlers
-from .admin import register_admin_handlers
-from .payment import register_payment_handlers
-from .referral import register_referral_handlers
+from .start import router as start_router
+from .admin import router as admin_router
+from .payment import router as payment_router
+from .referral import router as referral_router
 
 def register_handlers(dp):
     """Регистрация всех обработчиков"""
-    register_start_handlers(dp)
-    register_admin_handlers(dp)
-    register_payment_handlers(dp)
-    register_referral_handlers(dp)
+    dp.include_router(start_router)
+    dp.include_router(admin_router)
+    dp.include_router(payment_router)
+    dp.include_router(referral_router)
